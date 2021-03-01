27f019aef65cb234f6d88449d5a2b8169c5e2324
jacobsun <jacob.sun.meta@gmail.com>
2021-03-02 01:09:33 +0800

    fix asset link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 日语特殊的五段动词
+
+[https://ziox.xyz/special-godan-verbs-memo/](https://ziox.xyz/special-godan-verbs-memo/)
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
-const PRODUCTION_ASSETS_URL = '/'
+const PRODUCTION_ASSETS_URL = 'https://ziox.xyz/special-godan-verbs-memo/'
 
 const publicPath = () => process.env.mode === 'production' ? PRODUCTION_ASSETS_URL : '/'
 
